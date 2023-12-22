f7543ff09d4322de2fc198e72903afa01013e69e
Corien Bennink <benninkcorien@gmail.com>
2023-12-22 20:12:15 +0100

    readme

---
+++ corien_todolist.md
@@ -0,0 +1,6 @@
+# TODO - ical import into daily field, to prepopulate birthdays and recurring events from Google Calendar
+# Find a package that can do HTML to PDF - aargh.
+# Prettier layouts?
+# Extra note pages
+# more templates
+# Build a nicer index
--- generate-borb-pdf.py
+++ generate-borb-pdf.py
@@ -1,6 +1,5 @@
 import os
 import glob
-from pathlib import Path
 
 from borb.pdf import Document
 from borb.pdf import PDF
@@ -12,21 +11,18 @@ pdf_dir = "borb"
 
 os.makedirs(pdf_dir, exist_ok=True)
 
-# Loop through all HTML files in the html_dir
+
 for html_file in glob.glob(os.path.join(html_dir, "*.html")):
     base_name = os.path.basename(html_file).split(".")[0]
     pdf_file = os.path.join(pdf_dir, f"{base_name}.pdf")
     print(f"Working on {base_name}")
 
-    # read entire markdown file
     html_str: str = ""
     with open(html_file, "r", encoding="utf-8") as md_file_handle:
         html_str = md_file_handle.read()
 
-    # convert
     doc: Document = HTMLToPDF.convert_html_to_pdf(html_str)
     assert doc is not None
 
-    # write
     with open(pdf_file, "wb") as pdf_file_handle:
         PDF.dumps(pdf_file_handle, doc)
